1176eaec194502627280defba708df86f9eed901
Oompa man <ben@nackfamily.com>
2019-11-23 14:10:25 -0600

    Initial Commit

---
+++ Assets/Scripts/floorTextureFixer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class floorTextureFixer : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject[] floors = GameObject.FindGameObjectsWithTag("floor");
+        for(int i = 0; i < floors.Length; i++)
+        {
+            floors[i].GetComponent<Renderer>().material.SetTextureScale("_BaseMap", new Vector2(floors[i].transform.localScale.x, floors[i].transform.localScale.y));
+            Debug.Log(floors[i].name);
+        }
+    }
+}
---
+++ Assets/Scripts/rotPosCopy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class rotPosCopy : MonoBehaviour
+{
+    public Transform copy;
+    void Update()
+    {
+        transform.position = copy.position;
+        transform.rotation = copy.rotation;
+    }
+}
